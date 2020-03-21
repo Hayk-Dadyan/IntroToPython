28677d3be09cecdc0b34adc952cd761db10d2dbd
Hayk-Dadyan <61898502+Hayk-Dadyan@users.noreply.github.com>
2020-03-21 12:27:59 +0400

    Add files via upload

---
+++ Week1/Practical/Problem2.py
@@ -0,0 +1,11 @@
+#Problem 2
+course = "Intro To Python"
+student = "Hayk Dadyan"
+nickname = "Superman"
+grade = 100
+pythonExpert = True
+print("Course:",course)
+print("Student:",student)
+print("Nickname:",nickname)
+print("Grade:",grade)
+print("Python_expert:",pythonExpert)
