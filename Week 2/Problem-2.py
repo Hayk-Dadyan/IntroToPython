1ba04bc4b0200ce970f209515c3aaaeaf8cdefc6
Hayk-Dadyan <61898502+Hayk-Dadyan@users.noreply.github.com>
2020-03-21 12:30:15 +0400

    Delete Problem-2.py

--- Week 2/Problem-2.py
+++
@@ -1,7 +0,0 @@
-#Problem 2
-num1 = int(input("Input any number:" ))
-num2 = int(input("Input any number:" ))
-sum = num1 + num2
-print("First number:", num1)
-print("Second number:" ,num2)
-print("Sum:" ,sum)
