3e85d65c467b459a93e392460f3edf6e9a5b6fb5
Hayk-Dadyan <61898502+Hayk-Dadyan@users.noreply.github.com>
2020-03-21 12:30:31 +0400

    Delete Problem-5.py

--- Week 2/Problem-5.py
+++
@@ -1,9 +0,0 @@
-#Problem 5
-text = input("Type anything:" )
-text1 = text
-print("The given text:" ,text)
-print("# of a:",text.count('a'))
-print("# of b:",text.count('b'))
-print("# of c:",text.count('c'))
-print("# of d:",text.count('d'))
-print("# of e:",text.count('e'))
